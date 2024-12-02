e326c872d133eea9f4d79ffbd9ee3536cbd9ef52
ridhampatel10 <ridhampatel1002@gmail.com>
2024-12-02 20:04:08 +0530

    Update second _largest.c

--- second _largest.c
+++ second _largest.c
@@ -4,16 +4,20 @@ void main()
 {
   int a[5];
   int i=0,sl=-1,max=a[0];
-printf("\n enter array");
-scanf("%d",&a[i]);
-for(i=1; i<5; i++)
+{
+  for(i=0; i<5; i++)
+    {
+      printf("\n enter array");
+      scanf("%d",&a[i]);
+    }
+  for(i=1; i<5; i++)
     {
       if(a[i]>Max)
         {
           max=a[i];
         }
     }
-for(i=0; i<5; i++)
+  for(i=0; i<5; i++)
     {
        if(a[i]>sl && a[i]<max)
         {
